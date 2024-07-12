c5c9b6440466176b2754a79bcb6ed7c6275cc2a4
Visage <82640121+VisageDvachevsky@users.noreply.github.com>
2024-07-12 10:10:43 +0300

    12313

--- TGBot/bot.py
+++ TGBot/bot.py
@@ -5,14 +5,10 @@ from telebot import apihelper
 from TGmanager import BotAssistantManager
 from DB.database import save_rating, create_database
 from env import TELEGRAM_BOT_API_KEY
-import threading
+import time
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
 
 class TelegramBot:
     def __init__(self, api_key):
@@ -31,12 +27,7 @@ class TelegramBot:
         def send_welcome(message):
             try:
                 self.log_admin_activity("User started interaction", message.chat.id)
-                self.bot.reply_to(
-                    message,
-                    "Напишите сообщение, чтобы начать. "
-                    "Используйте /help для списка доступных команд.\n"
-                    "Версия: alpha:0.1"
-                )
+                self.bot.reply_to(message, "Добро пожаловать! Напишите сообщение, чтобы начать. Используйте /help для списка доступных команд.\nВерсия alpha0.1")
             except Exception as e:
                 self.handle_error(message, e)
 
@@ -70,79 +61,52 @@ class TelegramBot:
             try:
                 user_input = message.text
                 response, confidence, intent, intent_id, inputs = self.assistant_manager.process_input(user_input)
-                bot_reply = self.bot.reply_to(
-                    message,
-                    f"Бот: {response}\nУверенность: {confidence:.2f}\nНамерение: {intent}\n"
-                    f"intent_id: {intent_id}\ninputs: {inputs}"
-                )
+                bot_reply = self.bot.reply_to(message, f"Бот: {response}\nУверенность: {confidence:.2f}\nНамерение: {intent}\nintent_id: {intent_id}\ninputs: {inputs}")
 
                 markup = ReplyKeyboardMarkup(row_width=5, resize_keyboard=True, one_time_keyboard=True)
-                markup.add(
-                    KeyboardButton('1'), KeyboardButton('2'), 
-                    KeyboardButton('3'), KeyboardButton('4'), 
-                    KeyboardButton('5')
-                )
-                msg_rating = self.bot.send_message(
-                    message.chat.id, 
-                    "Оцените ответ (1-5):", 
-                    reply_markup=markup
-                )
+                markup.add(KeyboardButton('1'), KeyboardButton('2'), KeyboardButton('3'), KeyboardButton('4'), KeyboardButton('5'))
+                msg_rating = self.bot.send_message(message.chat.id, "Оцените ответ (1-5):", reply_markup=markup)
 
-                # Сохранение контекста для идентификации оценки
-                self.bot.register_next_step_handler(
-                    msg_rating, self.process_rating, user_input, response, intent, confidence, bot_reply, msg_rating
-                )
 
-                # Удаление сообщения пользователя через секунду
-                self.schedule_delete(message.chat.id, message.message_id, 1)
+                self.bot.register_next_step_handler(msg_rating, process_rating, user_input, response, intent, confidence, bot_reply, msg_rating)
+
+                time.sleep(1)
+                self.bot.delete_message(message.chat.id, message.message_id)
             except Exception as e:
                 self.handle_error(message, e)
 
-        def process_rating(self, message, user_input, response, intent, confidence, bot_reply, msg_rating):
+        def process_rating(message, user_input, response, intent, confidence, bot_reply, msg_rating):
             try:
                 rating = int(message.text)
                 markup = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
                 for intent_name, translation in self.INTENTS.items():
                     markup.add(KeyboardButton(intent_name))
 
-                msg_intent = self.bot.send_message(
-                    message.chat.id, 
-                    "Какое намерение вы ожидали?", 
-                    reply_markup=markup
-                )
+                msg_intent = self.bot.send_message(message.chat.id, "Какое намерение вы ожидали?", reply_markup=markup)
 
-                # Сохранение контекста для идентификации ожидаемого намерения
-                self.bot.register_next_step_handler(
-                    msg_intent, self.process_expected_intent, user_input, response, rating, intent, confidence, bot_reply, msg_rating, msg_intent
-                )
+                self.bot.register_next_step_handler(msg_intent, process_expected_intent, user_input, response, rating, intent, confidence, bot_reply, msg_rating, msg_intent)
 
-                # Удаление сообщения с запросом оценки через секунду
-                self.schedule_delete(message.chat.id, message.message_id, 1)
+                time.sleep(1)
+                self.bot.delete_message(message.chat.id, message.message_id)
             except Exception as e:
                 self.handle_error(message, e)
 
-        def process_expected_intent(self, message, user_input, response, rating, intent, confidence, bot_reply, msg_rating, msg_intent):
+        def process_expected_intent(message, user_input, response, rating, intent, confidence, bot_reply, msg_rating, msg_intent):
             try:
                 expected_intent = message.text
                 self.assistant_manager.save_user_feedback(user_input, response, rating, intent, expected_intent, confidence)
 
-                thank_you_msg = self.bot.send_message(
-                    message.chat.id, 
-                    "Спасибо за ваш отзыв!", 
-                    reply_markup=ReplyKeyboardRemove()
-                )
+                thank_you_msg = self.bot.send_message(message.chat.id, "Спасибо за ваш отзыв!", reply_markup=ReplyKeyboardRemove())
 
-                # Удаление всех соответствующих сообщений с интервалом
-                self.schedule_delete(message.chat.id, bot_reply.message_id, 1)
-                self.schedule_delete(message.chat.id, msg_rating.message_id, 2)
-                self.schedule_delete(message.chat.id, msg_intent.message_id, 3)
-                self.schedule_delete(message.chat.id, thank_you_msg.message_id, 4)
-                self.schedule_delete(message.chat.id, message.message_id, 5)
+
+                time.sleep(1)
+                self.bot.delete_message(message.chat.id, bot_reply.message_id)
+                self.bot.delete_message(message.chat.id, msg_rating.message_id)
+                self.bot.delete_message(message.chat.id, msg_intent.message_id)
+                self.bot.delete_message(message.chat.id, thank_you_msg.message_id)
+                self.bot.delete_message(message.chat.id, message.message_id)
             except Exception as e:
                 self.handle_error(message, e)
-
-    def schedule_delete(self, chat_id, message_id, delay):
-        threading.Timer(delay, lambda: self.bot.delete_message(chat_id, message_id)).start()
 
     def log_admin_activity(self, activity, user_id):
         logger.info(f"Admin Activity: {activity} by User ID: {user_id}")
@@ -159,11 +123,10 @@ class TelegramBot:
                 self.bot.polling(none_stop=True, interval=0)
             except apihelper.ApiException as e:
                 logger.error(f"API error occurred: {e}")
-                time.sleep(15)  # Avoid hitting API rate limits
+                time.sleep(15)  
             except Exception as e:
                 logger.critical(f"Critical error: {e}", exc_info=True)
-                time.sleep(15)  # Give some time before restarting polling
-
+                time.sleep(15) 
 
 if __name__ == '__main__':
     bot = TelegramBot(TELEGRAM_BOT_API_KEY)
